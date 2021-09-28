5d6bd000fbd1f85280c293bb358f71b023a2aa53
GhexterCortes <ghextershumies@gmail.com>
2021-09-28 19:16:01 +0800

    moved `newBot()` to new local module `scripts/bot.js`

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 // Modules
 const InternalVersion = require('./scripts/version');
 
-const Fs = require('fs');
 const Utility = require('./scripts/util');
 const FileConfig = require('./scripts/config');
 const FileLanguange = require('./scripts/language');
@@ -11,11 +10,7 @@ const FileResponse = require('./scripts/response');
 const prompt = require('prompt-sync')();
 
 const Discord = require('discord.js');
-
-const Mineflayer = require('mineflayer');
-const MineflayerCmd = require('mineflayer-cmd').plugin;
-const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
-const PvpBot = require('mineflayer-pvp').plugin;
+const Minecraft = require('./scripts/bot');
 
 const Startup = require('./scripts/startup')();
 
@@ -26,7 +21,6 @@ const parseConfig = new FileConfig();
 const parseLanguage = new FileLanguange();
 const parseResponse = new FileResponse();
 const Logger = require('./scripts/logger');
-const { rejects } = require('assert');
 
 parseConfig.location = './config/config.yml';
 parseLanguage.location = './config/language.yml';
@@ -36,10 +30,18 @@ let language = {};
 let response = {};
 let config = prefillConfig(parseConfig.parse());
 
-
 // Core Process
-var mcLoggedIn = false;
-newBot(config.player.name, config.server.ip, config.server.port, config.player.version);
+let bot = new Minecraft();
+    bot.playerName = config.player.name
+    bot.playerPvp = config.player.pvp.enabled
+    bot.playerChatDelay = config.player.chatDelay
+    bot.logMovements = config.debug.movements
+    bot.joinMessage = config.player.message
+    bot.serverIp = config.server.ip
+    bot.serverPort = config.server.port
+    bot.serverVersion = config.player.version;
+    bot.newBot();
+
 
 // Core Functions
 function prefillConfig(config){
@@ -65,203 +67,4 @@ function prefillConfig(config){
     language = parseLanguage.parse();
 
     return util.testMode(config);
-}
-
-function newBot(playerName = 'HiddenPlayer', serverIp = '127.0.0.1', serverPort = 25565, serverVersion = null){
-    
-    const mcLog = new Logger();
-        mcLog.defaultPrefix = 'Minecraft';
-    
-    mcLog.log("Creating new bot");
-
-    // Movements
-    const actions = ['forward', 'back', 'left', 'right'];
-    var lasttime = -1;
-    var moveinterval = 5;
-    var maxrandom = 5;
-    var moving = false;
-    var jump = true;
-    var onPVP = false;
-    var lastaction = null;
-
-    // Entities
-    let entity = null;
-
-    // Connection
-    switch (true){
-        case (mcLoggedIn):
-            mcLog.warn("Player already connected to a server");
-            return;
-        case (!config.player.enabled):
-            mcLog.warn("Player Disabled");
-            return;
-    }
-
-    mcLog.log(playerName + " is ready to connect!");
-
-    const validateCreds = new validateCredentials();
-    serverIp = validateCreds.host(serverIp);
-    serverPort = validateCreds.port(serverPort);
-
-    // Bot core
-    const connection = {
-        host: serverIp,
-        port: serverPort,
-        username: playerName,
-        version: serverVersion
-    };
-
-    mcLog.log('Connecting...');
-    mcLog.log(connection);
-    let bot = Mineflayer.createBot(connection);
-
-    bot.loadPlugin(MineflayerCmd);
-    bot.loadPlugin(pathfinder);
-    bot.loadPlugin(PvpBot);
-
-    mcLoggedIn = true;
-    let firstSpawn = false;
-
-    bot.on('kicked', (reason) => {
-        mcLog.warn(playerName + " left the game: " + reason)
-        endBot();
-    });
-    bot.on('error', (reason) => {
-        mcLog.error(playerName + " left the game: " + reason)
-        endBot();
-    });
-
-    bot.on('end', () => {
-        mcLog.warn(playerName + " Ended!");
-        reConnect();
-    });
-    bot.on('spawn', async () => {
-        if(!firstSpawn){
-            mcLog.log('First spawn of '+playerName);
-            firstSpawn = true;
-            if(config.player.message != null){
-                if(typeof config.player.message == 'object'){
-                    for (const value of Object.keys(config.player.message)) {
-                        
-                        setTimeout(() => { 
-                            bot.chat(config.player.message[value]); 
-                        }, config.player.chatDelay);
-                    }
-                } else {
-                    bot.chat(config.player.message.toString());
-                }
-            }
-        }
-        resetMoves();
-    });
-    bot.on('death', () => {
-        mcLog.warn(playerName + ' died!');
-        bot.emit('respawn');
-    });
-    bot.on('time', () => {
-        if(!config.player.enabled){
-            endBot();
-            return;
-        }
-
-        if(!firstSpawn) return;
-
-        entity = bot.nearestEntity();
-        if(entity && entity.position && entity.isValid && entity.type == 'mob' || entity && entity.position && entity.isValid && entity.type == 'player') bot.lookAt(entity.position.offset(0, 1.6, 0));
-        if(config.player.pvp.enabled){
-            if(entity && entity.kind && entity.isValid && entity.type == 'mob' && entity.kind.toLowerCase() == 'hostile mobs'){
-                onPVP = true;
-                bot.pvp.attack(entity);
-            } else {
-                onPVP = false;
-                bot.pvp.stop();
-            }
-        }
-
-        // Movements
-        if (lasttime < 0) {
-            lasttime = bot.time.age;
-            mcLog.log("Last time set!");
-            return;
-        }
-
-        let randomadd = Math.random() * maxrandom * 50;
-        let interval = moveinterval * 20 + randomadd;
-
-        if (bot.time.age - lasttime > interval) {
-            if (onPVP) { return; }
-
-            if (moving){
-                bot.setControlState(lastaction,false);
-                bot.deactivateItem();
-
-                moving = false;
-            } else{
-                lastaction = actions[Math.floor(Math.random() * actions.length)];
-                bot.setControlState(lastaction,true);
-                bot.activateItem();
-                
-                moving = true;
-                lasttime = bot.time.age;
-            }
-
-            if(config.debug.movements){
-                mcLog.log('Movements:');
-                mcLog.log({ age: bot.time.age, lasttime: lasttime, interval: interval, lastaction: lastaction, moving: moving, onPVP: onPVP });
-            }
-        }
-
-        //bot jump
-        if(jump){
-            bot.setControlState('jump', true);
-            bot.setControlState('jump', false);
-            jump = false
-        } else {
-            bot.setControlState('jump', false);
-            setTimeout(() => {
-                jump = true;
-            }, 1000);
-        }
-    });
-    bot.on('chat', (player, message) => {
-        mcLog.log('Chat: '+player+' > '+message);
-    });
-
-    // Functions
-    function validateCredentials(){
-        this.host = (host) => {
-            host = host.toString().trim();
-
-            if(host == '') throw new Error("Empty Minecraft player host: "+host);
-
-            return host;
-        }
-        this.port = (port) => {
-            port = parseInt(port);
-
-            if (!util.isNumber || port < 0 || port > 65535){
-                throw new Error("Invalid Minecraft player port: "+port);
-            }
-
-            return port;
-        }
-    }
-    function endBot(){
-        if(mcLoggedIn) return;
-        bot.quit();
-        bot.end();
-    }
-    function reConnect(){
-        setTimeout(() => {
-            mcLoggedIn = false;
-            newBot(config.player.name, config.server.ip, config.server.port, config.player.version);
-        }, parseInt(config.server.reconnectTimeout));
-    }
-    function resetMoves(){
-        lasttime = -1;
-        bot.pvp.stop();
-        if (lastaction != null) bot.setControlState(lastaction,false);
-        bot.deactivateItem();
-        moving = false;
-    }
 }
---
+++ scripts/bot.js
@@ -0,0 +1,215 @@
+const Logger = require('./logger');
+const Utility = require('./util');
+const Mineflayer = require('mineflayer');
+const MineflayerCmd = require('mineflayer-cmd').plugin;
+const { pathfinder, Movements, goals } = require('mineflayer-pathfinder');
+const PvpBot = require('mineflayer-pvp').plugin;
+
+const util = new Utility();
+const mcLog = new Logger();
+module.exports = function (){
+    this.mcLoggedIn = false;            // If the has logged in
+                                        //
+    this.serverIp = '';                 // Server IP 
+    this.serverPort = 25565;            // Server port
+    this.serverVersion = null;          // Server version
+    this.playerName = 'HiddenPlayer';   // Player name
+    this.playerChatDelay = 1000;        // Player chats delay
+    this.playerPvp = false;             // Enable player pvp
+    this.joinMessage = 'hello';         // onJoin player message (Sting || Object)
+
+    this.lastTime = -1;                                     // Last recorded time
+    this.actions = ['forward', 'back', 'left', 'right'];    // Random actions
+    this.moveInterval = 5;                                  // Movement interval
+    this.maxRandom = 5;                                     // Max Movement interval
+    this.lastAction = null;                                 // Last recorded action
+    
+    this.consolePrefix = 'Minecraft';                       // Console prefix
+    this.logMovements = false;       
+    this.disabled = false;                       // Log movements to console
+
+    var onPvp = false;
+    var moving = false;
+    var jump = true;
+
+    this.newBot = () => {
+        mcLog.defaultPrefix = this.consolePrefix;
+        mcLog.log("Creating new bot");
+
+        // Connection
+        if(this.alreadyConnected()) { mcLog.warn("Player already connected to a server"); return; }
+        if(this.isDisabled()) { mcLog.warn("Player Disabled"); return; }
+    
+        let serverIp = this.validateCredsHost(this.serverIp);
+        let serverPort = this.validateCredsPort(this.serverPort);
+        let playerName = this.playerName;
+        let serverVersion = this.serverVersion;
+
+        let entity = null;
+        let chatDelay = this.playerChatDelay;
+
+        mcLog.log(playerName + " is ready to connect!");
+
+        // Bot
+        const connection = { host: serverIp, port: serverPort, username: playerName, version: serverVersion };
+    
+        mcLog.log('Connecting...');
+        mcLog.log(connection);
+        var bot = Mineflayer.createBot(connection);
+    
+        bot.loadPlugin(MineflayerCmd);
+        bot.loadPlugin(pathfinder);
+        bot.loadPlugin(PvpBot);
+    
+        this.mcLoggedIn = true;
+        let firstSpawn = false;
+    
+        bot.on('kicked', (reason) => {
+            mcLog.warn(playerName + " left the game: " + reason);
+            this.endBot(bot);
+        });
+        bot.on('error', (reason) => {
+            mcLog.error(playerName + " left the game: " + reason);
+            this.endBot(bot);
+        });
+    
+        bot.on('end', () => {
+            mcLog.warn(playerName + " Ended!");
+            this.reConnect(bot);
+        });
+        bot.on('spawn', async () => {
+            if(!firstSpawn){
+                mcLog.log('First spawn of '+playerName);
+                firstSpawn = true;
+                let joinMessage = this.joinMessage;
+                if(joinMessage != null){
+                    if(typeof joinMessage == 'object'){
+                        for (const value of Object.keys(joinMessage)) {
+                            
+                            setTimeout(() => { 
+                                bot.chat(joinMessage[value]); 
+                            }, chatDelay);
+                        }
+                    } else {
+                        bot.chat(joinMessage.toString());
+                    }
+                }
+            }
+            this.resetMoves(bot);
+        });
+        bot.on('death', () => {
+            mcLog.warn(playerName + ' died!');
+            bot.emit('respawn');
+        });
+        bot.on('time', () => {
+            if(this.isDisabled()){ this.endBot(bot); return; }
+            if(!firstSpawn) return;
+    
+            entity = bot.nearestEntity();
+            if(entity && entity.position && entity.isValid && entity.type == 'mob' || entity && entity.position && entity.isValid && entity.type == 'player') bot.lookAt(entity.position.offset(0, 1.6, 0));
+            if(this.playerPvp){
+                if(entity && entity.kind && entity.isValid && entity.type == 'mob' && entity.kind.toLowerCase() == 'hostile mobs'){
+                    onPvp = true;
+                    bot.pvp.attack(entity);
+                } else {
+                    onPvp = false;
+                    bot.pvp.stop();
+                }
+            }
+    
+            // Movements
+            if (this.lastTime < 0) {
+                this.lastTime = bot.time.age;
+                mcLog.log("Last time set!");
+                return;
+            }
+    
+            let randomadd = Math.random() * this.maxRandom * 50;
+            let interval = this.moveInterval * 20 + randomadd;
+    
+            if (bot.time.age - this.lastTime > interval) {
+                if (onPvp) { return; }
+    
+                if (moving){
+                    bot.setControlState(this.lastAction,false);
+                    bot.deactivateItem();
+    
+                    moving = false;
+                } else{
+                    this.lastAction = this.actions[Math.floor(Math.random() * Object.keys(this.actions).length)];
+                    bot.setControlState(this.lastAction,true);
+                    bot.activateItem();
+                    
+                    moving = true;
+                    this.lastTime = bot.time.age;
+                }
+    
+                if(this.logMovements){
+                    mcLog.log('Movements:');
+                    mcLog.log({ age: bot.time.age, lastTime: this.lastTime, interval: interval, lastAction: this.lastAction, moving: moving, onPVP: onPVP });
+                }
+            }
+    
+            //bot jump
+            if(jump){
+                bot.setControlState('jump', true);
+                bot.setControlState('jump', false);
+                jump = false
+            } else {
+                bot.setControlState('jump', false);
+                setTimeout(() => {
+                    jump = true;
+                }, 1000);
+            }
+        });
+        bot.on('chat', (player, message) => {
+            mcLog.log('Chat: '+player+' > '+message);
+        });
+    }
+    this.alreadyConnected = () => {
+        if(this.mcLoggedIn) return true;
+        return false;
+    }
+
+    this.isDisabled = () => {
+        if(this.disabled) return true;
+        return false;
+    }
+
+    this.endBot = (bot) => {
+        if(this.mcLoggedIn || !bot) return;
+        bot.quit();
+        bot.end();
+    }
+    this.reConnect = (bot) => {
+        if(!bot) return;
+        setTimeout(() => {
+            this.mcLoggedIn = false;
+            newBot(this.playerName, this.serverIp, this.serverPort, this.serverVersion);
+        }, parseInt(this.reconnectTimeout));
+    }
+    this.resetMoves = (bot) => {
+        if(!bot) return;
+        this.lastTime = -1;
+        bot.pvp.stop();
+        if (this.lastAction != null) bot.setControlState(this.lastAction,false);
+        bot.deactivateItem();
+        moving = false;
+    }
+
+    this.validateCredsHost = (serverIp) => {
+        serverIp = serverIp.toString().trim();
+
+        if(serverIp == '') throw new Error("Empty Minecraft player host: "+serverIp);
+
+        return serverIp;
+    }
+    this.validateCredsPort = (serverPort) => {
+        serverPort = parseInt(serverPort);
+
+        if (!util.isNumber || serverPort < 0 || serverPort > 65535){
+            throw new Error("Invalid Minecraft player port: "+serverPort);
+        }
+        return serverPort;
+    }
+}
